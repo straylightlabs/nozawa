4790f450cbe395071e73cc60304ecfe2407fe713
Keiji Maekawa <m@keijak.com>
2016-03-06 10:45:14 +0900

    Refactoring: delete photo image picker from Detect Item View

--- Nozawa/DetectItemViewController.swift
+++ Nozawa/DetectItemViewController.swift
@@ -10,15 +10,13 @@ import UIKit
 import AVFoundation
 import SnapKit
 
-class DetectItemViewController: CameraBaseViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, AVCaptureVideoDataOutputSampleBufferDelegate {
+class DetectItemViewController: CameraBaseViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
 
     var photoImageView: UIImageView!
-    var photoPickerButton: UIButton!
     var detectionResultLabel: UILabel!
 
-    var pickingImage = false
-
-    let imagePicker = UIImagePickerController()
+    var captureDebugCounter = 0
+    var processingCapturedImage = false
 
     // MARK: Lifecycle
 
@@ -26,53 +24,52 @@ class DetectItemViewController: CameraBaseViewController, UIImagePickerControlle
         super.viewDidLoad()
 
         self.loadSubviews()
-
-        self.imagePicker.delegate = self
-
-        self.photoPickerButton.addTarget(self, action: "photoPickerButtonTapped:", forControlEvents: .TouchDown)
     }
 
     override func viewWillAppear(animated: Bool) {
-        if !self.pickingImage {
-            self.showLiveCameraView()
-        }
+        self.showLiveCameraView()
+
+        super.viewWillAppear(animated)
     }
 
-    // MARK: Actions
+    // MARK: AVCaptureVideoDataOutputSampleBufferDelegate
 
-    func photoPickerButtonTapped(sender: UIButton) {
-        self.pickingImage = true
+    func captureOutput(captureOutput: AVCaptureOutput!, didOutputSampleBuffer sampleBuffer: CMSampleBuffer!, fromConnection connection: AVCaptureConnection!) {
+        connection.videoOrientation = .Portrait
+        if let device = self.cameraDevice {
+            if device.adjustingFocus || device.adjustingExposure || device.adjustingWhiteBalance || self.processingCapturedImage {
+                return
+            }
+            self.processingCapturedImage = true
+            if let img = self.imageFromSampleBuffer(sampleBuffer) {
+                let conversionStart = NSDate()
 
-        self.imagePicker.allowsEditing = false
-        self.imagePicker.sourceType = .PhotoLibrary
-        self.presentViewController(self.imagePicker, animated: true, completion: nil)
-    }
+                // TODO(maekawa): Merge drawKeypoints and findMatches so that keypoints detection runs only once.
+                let processedImage = NZImageInternal().drawKeypoints(img)
+                dispatch_async(dispatch_get_main_queue(), {
+                    self.photoImageView?.image = processedImage
+                })
 
-    // MARK: UIImagePickerControllerDelegate
+                let detectionResult = self.findMatches(img)
+                dispatch_async(dispatch_get_main_queue(), {
+                    self.detectionResultLabel.text = detectionResult
+                })
 
-    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
-        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
-            self.photoImageView.image = NZImageInternal().drawKeypoints(pickedImage)
+                let elapsedSec = NSDate().timeIntervalSinceDate(conversionStart) as Double
+                print("capture\(self.captureDebugCounter++): size = \(img.size), elapsed = \(elapsedSec*1000)[ms]")
+            }
+            self.processingCapturedImage = false
         }
-        self.dismissViewControllerAnimated(true, completion: nil)
     }
 
     // MARK: Private
 
-    func loadSubviews() {
+    private func loadSubviews() {
         self.view.backgroundColor = UIColor.whiteColor()
 
         let cameraView = self.loadCameraView()
         cameraView.snp_makeConstraints{ make in
             make.top.left.right.bottom.equalTo(0)
-        }
-
-        self.photoPickerButton = UIButton(type: .System)
-        self.photoPickerButton.setTitle("Camera Roll", forState: .Normal)
-        self.view.addSubview(self.photoPickerButton)
-        self.photoPickerButton.snp_makeConstraints{ make in
-            make.bottom.equalTo(self.view.snp_bottomMargin).offset(-8)
-            make.trailing.equalTo(self.view.snp_trailingMargin)
         }
 
         self.photoImageView = UIImageView()
@@ -89,13 +86,13 @@ class DetectItemViewController: CameraBaseViewController, UIImagePickerControlle
         self.detectionResultLabel.backgroundColor = UIColor(white: 1, alpha: 0.3)
         self.view.addSubview(self.detectionResultLabel)
         self.detectionResultLabel.snp_makeConstraints{ make in
-            make.bottom.equalTo(self.photoPickerButton.snp_top).offset(-8)
+            make.bottom.equalTo(self.view.snp_bottom).offset(-8)
             make.left.equalTo(8)
             make.right.equalTo(-8)
         }
     }
 
-    func showLiveCameraView() {
+    private func showLiveCameraView() {
         self.startCameraSession()
 
         // Set output.
@@ -107,32 +104,7 @@ class DetectItemViewController: CameraBaseViewController, UIImagePickerControlle
         }
     }
 
-    var captureDebugCounter = 0
-    var processingCapturedImage = false
-    func captureOutput(captureOutput: AVCaptureOutput!, didOutputSampleBuffer sampleBuffer: CMSampleBuffer!, fromConnection connection: AVCaptureConnection!) {
-        connection.videoOrientation = .Portrait
-        if let device = self.cameraDevice {
-            if device.adjustingFocus || device.adjustingExposure || device.adjustingWhiteBalance || self.processingCapturedImage {
-                return
-            }
-            self.processingCapturedImage = true
-            if let img = self.imageFromSampleBuffer(sampleBuffer) {
-                let conversionStart = NSDate()
-                let processedImage = NZImageInternal().drawKeypoints(img)
-                let detectionResult = self.findMatches(img)
-                let elapsedSec = NSDate().timeIntervalSinceDate(conversionStart) as Double
-                print("capture\(self.captureDebugCounter++): size = \(img.size), elapsed = \(elapsedSec*1000)[ms]")
-
-                dispatch_async(dispatch_get_main_queue(), {
-                    self.photoImageView?.image = processedImage
-                    self.detectionResultLabel.text = detectionResult
-                })
-            }
-            self.processingCapturedImage = false
-        }
-    }
-
-    func imageFromSampleBuffer(sampleBuffer: CMSampleBuffer) -> UIImage? {
+    private func imageFromSampleBuffer(sampleBuffer: CMSampleBuffer) -> UIImage? {
         if let pixelBuffer : CVPixelBufferRef = CMSampleBufferGetImageBuffer(sampleBuffer) {
             let ciimg = CIImage(CVPixelBuffer: pixelBuffer)
             let ciContext:CIContext = CIContext(options: nil)

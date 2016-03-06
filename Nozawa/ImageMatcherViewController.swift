b5bffcbd2bb4a802203180880109aeb5ae79d2a7
Ryo Kawaguchi <ryo@straylight.jp>
2016-03-06 18:10:03 +0900

    Make ImageMatcherView scrollable (90% working)

--- Nozawa/ImageMatcherViewController.swift
+++ Nozawa/ImageMatcherViewController.swift
@@ -5,18 +5,11 @@ import UIKit
 import AVFoundation
 import SnapKit
 
-class ImageMatcherViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ImageMatcherViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITableViewDelegate, UITableViewDataSource {
 
-  let numSubImageViews = 4
-
-  var photoImageView: UIImageView!
-  var subImageViews: Array<UIImageView>! = []
-  var photoPickerButton: UIButton!
-
-  var pickingImage = false
-
+  let tableView = UITableView()
+  var matches: [UIImage] = []
   let imageMatcher = NZImageMatcher()
-
   let imagePicker = UIImagePickerController()
 
   // MARK: Lifecycle
@@ -24,26 +17,17 @@ class ImageMatcherViewController: UIViewController, UIImagePickerControllerDeleg
   override func viewDidLoad() {
     super.viewDidLoad()
 
-    self.loadSubviews()
-
     self.imagePicker.delegate = self
 
-    for item in ImageItem.items {
-        imageMatcher.addImage(item.image, name: item.name)
-    }
+    self.loadPresetData()
+    self.loadSubviews()
 
-    self.photoPickerButton.addTarget(self, action: "photoPickerButtonTapped:", forControlEvents: .TouchDown)
-  }
-
-  override func viewWillAppear(animated: Bool) {
-
+    self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "addButtonTapped:")
   }
 
   // MARK: Actions
 
-  func photoPickerButtonTapped(sender: UIButton) {
-    self.pickingImage = true
-
+  func addButtonTapped(sender: UIButton) {
     self.imagePicker.allowsEditing = false
     self.imagePicker.sourceType = .PhotoLibrary
     self.presentViewController(self.imagePicker, animated: true, completion: nil)
@@ -53,53 +37,51 @@ class ImageMatcherViewController: UIViewController, UIImagePickerControllerDeleg
 
   func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
     if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
-      //self.photoImageView.image = pickedImage
-      var similarImageResults : Array = imageMatcher.getSimilarImages(pickedImage)
-      for i in 0..<numSubImageViews {
-        let subImageView = subImageViews[i]
-        subImageView.image = nil
-        if i < similarImageResults.count {
-          let imageResult = similarImageResults[i] as! ImageResult
-          subImageView.image = imageResult.debugImage
-        }
-      }
-      imageMatcher.addImage(pickedImage, name: "")
+      let similarImages = imageMatcher.getSimilarImages(pickedImage) as! [ImageResult]
+      self.displayMatches(similarImages)
+      self.imageMatcher.addImage(pickedImage, name: "")
     }
     self.dismissViewControllerAnimated(true, completion: nil)
   }
 
+  // MARK: UITableViewDataSource
+
+  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return self.matches.count
+  }
+
+  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCellWithIdentifier("cell")!
+    cell.imageView!.image = self.matches[indexPath.row]
+    return cell
+  }
+
   // MARK: Private
+
+  func loadPresetData() {
+    for item in ImageItem.items {
+      imageMatcher.addImage(item.image, name: item.name)
+    }
+  }
+
+  func displayMatches(matches: [ImageResult]) {
+    self.matches = []
+    for match in matches {
+      self.matches.append(match.debugImage)
+    }
+    self.tableView.reloadData()
+  }
 
   func loadSubviews() {
     self.view.backgroundColor = UIColor.whiteColor()
 
-    self.photoPickerButton = UIButton(type: .System)
-    self.photoPickerButton.setTitle("Camera Roll", forState: .Normal)
-    self.view.addSubview(self.photoPickerButton)
-    self.photoPickerButton.snp_makeConstraints{ make in
-      make.bottom.equalTo(self.view.snp_bottomMargin).offset(-8)
-      make.trailing.equalTo(self.view.snp_trailingMargin)
-    }
-
-//    self.photoImageView = UIImageView()
-//    self.photoImageView.backgroundColor = UIColor.grayColor()
-//    self.photoImageView.contentMode = .ScaleAspectFill
-//    self.view.addSubview(self.photoImageView)
-//    self.photoImageView.snp_makeConstraints{ make in
-//      make.left.right.equalTo(0)
-//      make.height.equalTo(self.view.snp_height).multipliedBy(0.5)
-//    }
-
-    for i in 0..<numSubImageViews {
-      let subImageView : UIImageView = UIImageView()
-      subImageView.contentMode = .ScaleAspectFit
-      self.view.addSubview(subImageView)
-      subImageView.snp_makeConstraints{make in
-        make.left.right.equalTo(0)
-        make.top.equalTo(180 * i + 80)
-        make.height.equalTo(230)
-      }
-      subImageViews.append(subImageView)
+    self.tableView.delegate = self
+    self.tableView.dataSource = self
+    self.tableView.rowHeight = 160
+    self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    self.view.addSubview(self.tableView)
+    self.tableView.snp_makeConstraints{ make in
+      make.edges.equalTo(self.view)
     }
   }
 

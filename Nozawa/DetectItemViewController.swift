10aaaf816f48a35c0585e919898afc7be2c0155b
Keiji Maekawa <m@keijak.com>
2016-03-07 09:51:47 +0900

    Fix lifecycle management of camera and background jobs.

--- Nozawa/DetectItemViewController.swift
+++ Nozawa/DetectItemViewController.swift
@@ -16,14 +16,15 @@ class DetectItemViewController: CameraBaseViewController, AVCaptureVideoDataOutp
     var keypointsOverlayView: UIImageView!
     var detectionResultLabel: UILabel!
     var detectionDebugViews: [UIImageView] = []
-    let maxDetectionDebugViews = 4
+    static let maxDetectionDebugViews = 4
 
     var processingDrawKeypoints = false
     var processingFindMatches = false
 
-    let videoOutput = AVCaptureVideoDataOutput()
-    let dispatchQueueVideoCapture = dispatch_queue_create("videocapture", nil)
-    let dispatchQueueMatching = dispatch_queue_create("matching", nil)
+    var videoOutput: AVCaptureVideoDataOutput!
+    let dispatchQueueVideoCapture = dispatch_queue_create("videocapture", DISPATCH_QUEUE_CONCURRENT)
+    let dispatchQueueDrawKeypoints = dispatch_queue_create("drawkeypoints", DISPATCH_QUEUE_CONCURRENT)
+    let dispatchQueueFindMatches = dispatch_queue_create("findmatches", DISPATCH_QUEUE_CONCURRENT)
 
     // MARK: Lifecycle
 
@@ -34,15 +35,15 @@ class DetectItemViewController: CameraBaseViewController, AVCaptureVideoDataOutp
     }
 
     override func viewWillAppear(animated: Bool) {
-        self.showLiveCameraView()
+        self.startCameraSession()
 
         super.viewWillAppear(animated)
     }
 
     override func viewDidDisappear(animated: Bool) {
-        super.viewDidDisappear(true)
+        super.viewDidDisappear(animated)
 
-        videoOutput.setSampleBufferDelegate(nil, queue: nil)
+        self.stopCameraSession()
     }
 
     override func viewDidLayoutSubviews() {
@@ -56,64 +57,63 @@ class DetectItemViewController: CameraBaseViewController, AVCaptureVideoDataOutp
         }
     }
 
+    // MARK: CameraBaseViewController
+
+    override func startCameraSession() -> Bool {
+        if !super.startCameraSession() {
+            return false
+        }
+
+        // Set output.
+        videoOutput = AVCaptureVideoDataOutput()
+        videoOutput.setSampleBufferDelegate(self, queue: dispatchQueueVideoCapture)
+        videoOutput.alwaysDiscardsLateVideoFrames = true
+        if self.captureSession.canAddOutput(videoOutput) {
+            self.captureSession.addOutput(videoOutput)
+        }
+
+        self.processingDrawKeypoints = false
+        self.processingFindMatches = false
+
+        return true
+    }
+
+    override func stopCameraSession() {
+        self.captureSession.removeOutput(videoOutput)
+        super.stopCameraSession()
+    }
+
     // MARK: AVCaptureVideoDataOutputSampleBufferDelegate
 
     func captureOutput(captureOutput: AVCaptureOutput!, didOutputSampleBuffer sampleBuffer: CMSampleBuffer!, fromConnection connection: AVCaptureConnection!) {
-        if self.processingDrawKeypoints { return }
-        self.processingDrawKeypoints = true
-        defer { self.processingDrawKeypoints = false }
-
         // Set video orientation to fix rotation of the captured image.
         connection.videoOrientation = .Portrait
 
-        if let img = self.imageFromSampleBuffer(sampleBuffer) {
-            let drawKeypointsStart = NSDate()
-            let processedImage = NZImageMatcher.drawKeypoints(img)
-            let drawKeypointsElapsed = NSDate().timeIntervalSinceDate(drawKeypointsStart) as Double
-            print("drawKeypoints: \(drawKeypointsElapsed*1000)[ms]")
+        if let image = self.imageFromSampleBuffer(sampleBuffer) {
+            if !self.processingDrawKeypoints {
+                dispatch_async(dispatchQueueDrawKeypoints, { [weak self] in
+                    self?.processingDrawKeypoints = true
+                    defer { self?.processingDrawKeypoints = false }
 
-            dispatch_async(dispatch_get_main_queue(), {
-                self.keypointsOverlayView!.image = processedImage
-            })
+                    self?.doDrawKeypoints(image)
+                })
+            }
+            if !self.processingFindMatches {
+                dispatch_async(dispatchQueueFindMatches, { [weak self] in
+                    self?.processingFindMatches = true
+                    defer { self?.processingFindMatches = false }
 
-            if self.processingFindMatches { return }
-            dispatch_async(self.dispatchQueueMatching, {
-                self.processingFindMatches = true
-                defer { self.processingFindMatches = false }
-
-                if let device = self.cameraDevice {
-                    if !device.adjustingFocus && !device.adjustingExposure && !device.adjustingWhiteBalance {
-                        let findMatchesStart = NSDate()
-                        var similarImages = ImageItem.imageMatcher.getSimilarImages(img, crop: false) as! [ImageResult]
-                        let findMatchesElapsed = NSDate().timeIntervalSinceDate(findMatchesStart) as Double
-                        print("findMatches: \(findMatchesElapsed*1000)[ms]")
-
-                        if similarImages.count > self.maxDetectionDebugViews {
-                            similarImages = Array(similarImages[0..<3])
+                    if let device = self?.cameraDevice {
+                        if !device.adjustingFocus && !device.adjustingExposure && !device.adjustingWhiteBalance {
+                            self?.doFindMatches(image)
                         }
-                        dispatch_async(dispatch_get_main_queue(), {
-                            var detectionResult = "Detection Result:"
-                            for image in similarImages {
-                                detectionResult += String(format: "\n%@ = %.2f", image.name, image.similarity)
-                            }
-                            self.detectionResultLabel.text = detectionResult
-
-                            for var i = 0; i < similarImages.count; i++ {
-                                self.detectionDebugViews[i].image = similarImages[i].debugImage
-                                self.detectionDebugViews[i].hidden = false
-                            }
-                            for var i = similarImages.count; i < self.maxDetectionDebugViews; i++ {
-                                self.detectionDebugViews[i].hidden = true
-                            }
-                        })
                     }
-                }
-            })
+                })
+            }
         }
     }
 
     // MARK: Private
-
     private func loadSubviews() {
         self.view.backgroundColor = UIColor.whiteColor()
 
@@ -147,7 +147,7 @@ class DetectItemViewController: CameraBaseViewController, AVCaptureVideoDataOutp
             make.right.equalTo(-8)
         }
 
-        for i in 0...(maxDetectionDebugViews - 1) {
+        for i in 0...(DetectItemViewController.maxDetectionDebugViews - 1) {
             let subImageView : UIImageView = UIImageView()
             subImageView.contentMode = .ScaleAspectFit
             detectionDebugViews.append(subImageView)
@@ -161,17 +161,6 @@ class DetectItemViewController: CameraBaseViewController, AVCaptureVideoDataOutp
         }
     }
 
-    private func showLiveCameraView() {
-        self.startCameraSession()
-
-        // Set output.
-        videoOutput.setSampleBufferDelegate(self, queue: self.dispatchQueueVideoCapture)
-        videoOutput.alwaysDiscardsLateVideoFrames = true
-        if self.captureSession.canAddOutput(videoOutput) {
-            self.captureSession.addOutput(videoOutput)
-        }
-    }
-
     private func imageFromSampleBuffer(sampleBuffer: CMSampleBuffer) -> UIImage? {
         if let pixelBuffer : CVPixelBufferRef = CMSampleBufferGetImageBuffer(sampleBuffer) {
             let ciimg = CIImage(CVPixelBuffer: pixelBuffer)
@@ -180,5 +169,43 @@ class DetectItemViewController: CameraBaseViewController, AVCaptureVideoDataOutp
             return UIImage(CGImage: cgimg)
         }
         return nil
+    }
+
+    private func doDrawKeypoints(image: UIImage) {
+        let drawKeypointsStart = NSDate()
+        let processedImage = NZImageMatcher.drawKeypoints(image)
+        let drawKeypointsElapsed = NSDate().timeIntervalSinceDate(drawKeypointsStart) as Double
+        print("drawKeypoints: \(drawKeypointsElapsed*1000)[ms]")
+
+        dispatch_async(dispatch_get_main_queue(), { [weak self] in
+            self?.keypointsOverlayView!.image = processedImage
+        })
+    }
+
+    private func doFindMatches(image: UIImage) {
+        let findMatchesStart = NSDate()
+        var similarImages = ImageItem.imageMatcher.getSimilarImages(image, crop: false) as! [ImageResult]
+        let findMatchesElapsed = NSDate().timeIntervalSinceDate(findMatchesStart) as Double
+        print("findMatches: \(findMatchesElapsed*1000)[ms]")
+
+        let maxNum = DetectItemViewController.maxDetectionDebugViews
+        if similarImages.count > maxNum {
+            similarImages = Array(similarImages[0..<maxNum])
+        }
+
+        dispatch_async(dispatch_get_main_queue(), { [weak self] in
+            var detectionResult = "Detection Result:"
+            for image in similarImages {
+                detectionResult += String(format: "\n%@ = %.2f", image.name, image.similarity)
+            }
+            self?.detectionResultLabel.text = detectionResult
+            for var i = 0; i < similarImages.count; i++ {
+                self?.detectionDebugViews[i].image = similarImages[i].debugImage
+                self?.detectionDebugViews[i].hidden = false
+            }
+            for var i = similarImages.count; i < maxNum; i++ {
+                self?.detectionDebugViews[i].hidden = true
+            }
+        })
     }
 }
